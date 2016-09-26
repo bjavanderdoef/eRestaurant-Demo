090aa31e930462804676af596ba4fb37ab167fa0
bjavanderdoef <bvanderdoef1@studentmail.nait.ca>
2016-09-26 16:53:05 -0600

    Create eRestaurant Website & Solution

---
+++ eRestaurant/DAL/DAL.cs
@@ -0,0 +1,23 @@
+﻿using eRestaurant.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eRestaurant.DAL
+{
+    // : DbContext means that my class inherits from the DbContext class
+    internal class RestaurantContex : DbContext
+    {
+        // : base(string) is the constructor of the DbContext class that I call before running the body of my own construction.
+        // Hooking up constructors to call other constructors is called "Constructor Chaining"
+        public RestaurantContex() : base("name=EatIn") { }
+
+        public DbSet<SpecialEvent> SpecialEvents { get; set; }
+        public DbSet<Reservation> Reservations { get; set; }
+        public DbSet<MenuCategory> MenuCategories { get; set; }
+        public DbSet<Item> Items { get; set; }
+    }
+}
